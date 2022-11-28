e2b4ac612ed808a6ed041e03f1e6f8468056eb9d
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2022-11-28 13:30:23 -0500

    Added validation to CoverType model

--- Models/CoverType.cs
+++ Models/CoverType.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace AlexsBooks.Models
 {
     public class CoverType
     {
+        [Key]
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "Cover Type")]
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
